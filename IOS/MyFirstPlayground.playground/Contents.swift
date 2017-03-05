e06cd62f202298f76d84703d281388d8a144c800
Joseph Zoland <joeyzoland@gmail.com>
2017-03-05 14:49:37 -0800

    Starting IOS stack

---
+++ IOS/MyFirstPlayground.playground/Contents.swift
@@ -0,0 +1,139 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//var str = "Hello, playground"
+//print (str)
+//
+
+
+//var myVariable = 42
+//myVariable = 50
+//let myConstant = 42
+
+
+//let implicitInteger = 70
+//let implicitDouble = 70.0
+//let explicitDouble: Double = 70
+
+
+//LOOKS GOOD
+//let apples = 3
+//let oranges = 5
+//let appleSummary = "I have \(apples) apples."
+//let fruitSummary = "I have \(apples + oranges) pieces of fruit."
+
+
+//var shoppingList = ["catfish", "water", "tulips", "blue paint"]
+//shoppingList[1] = "bottle of water"
+//
+//var occupations = [
+//    "Malcolm": "Captain",
+//    "Kaylee": "Mechanic",
+//]
+//occupations["Jayne"] = "Public Relations"
+
+
+//let emptyArray = [String]()
+//let emptyDictionary = [String: Float]()
+
+
+//let label = "The width is "
+//let width = 94
+//let widthLabel = label + String(width)
+//
+//print (widthLabel)
+
+
+//var optionalString: String? = "Hello"
+//print(optionalString == nil)
+//
+//var optionalName: String? = "John Appleseed"
+//var greeting = "Hello!"
+//if let name = optionalName {
+//    greeting = "Hello, \(name)"
+//}
+
+
+//let nickName: String? = nil
+//let fullName: String = "John Appleseed"
+//let informalGreeting = "Hi \(nickName ?? fullName)"
+
+
+//LOOKS GOOD
+//var optionalString: String? = "Hello"
+//print(optionalString == nil)
+//
+//var optionalName: String? = "John Appleseed"
+//var greeting = "Hello!"
+//if let name = optionalName {
+//    greeting = "Hello, \(name)"
+//}
+
+
+//LOOKS GOOD
+//let individualScores = [75, 43, 103, 87, 12]
+//var teamScore = 0
+//for score in individualScores {
+//    if score > 50 {
+//        teamScore += 3
+//    } else {
+//        teamScore += 1
+//    }
+//}
+//print(teamScore)
+
+
+//LOOKS GOOD
+//let vegetable = "red pepper"
+//switch vegetable {
+//case "celery":
+//    print("Add some raisins and make ants on a log.")
+//case "cucumber", "watercress":
+//    print("That would make a good tea sandwich.")
+//case let x where x.hasSuffix("pepper"):
+//    print("Is it a spicy \(x)?")
+//default:
+//    print("Everything tastes good in soup.")
+//}
+
+
+//LOOKS GOOD
+//let interestingNumbers = [
+//    "Prime": [2, 3, 5, 7, 11, 13],
+//    "Fibonacci": [1, 1, 2, 3, 5, 8],
+//    "Square": [1, 4, 9, 16, 25],
+//]
+//var largest = 0
+//for (kind, numbers) in interestingNumbers {
+//    for number in numbers {
+//        if number > largest {
+//            largest = number
+//        }
+//    }
+//}
+//print(largest)
+
+
+//var n = 2
+//while n < 100 {
+//    n = n * 2
+//}
+//print(n)
+//
+//var m = 2
+//repeat {
+//    m = m * 2
+//} while m < 100
+//print(m)
+
+
+//NOT SURE
+//var total = 0
+//for i in 0..<4 {
+//    total += i
+//}
+//print(total)
+
+
+//STOPPED AT FUNCTIONS AND CLOSURES
---
+++ IOS/stringconcat.playground/Contents.swift
@@ -0,0 +1,21 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+let (x, y) = (1, 2)
+print (x)
+print (y)
+
+//Swift is strongly typed, meaning variable types cannot change after creation
+//Use constants for everything that doesn't vary, in which case, use variables
+
+//var name = "Anakin"            // Type inferred as String based on the value "Anakin"
+//name = 42                      // THIS IS STILL NOT ALLOWED since name is a "String" type
+
+//// Declare name as a constant because we anticipate that it will NOT change.
+//let name: String = "Anakin Skywalker"
+//// Note that just like variables, constants also have types
+//// Now we cannot change the name:
+//name = "Darth Vader"        // THIS WON'T WORK because name is a constant so we CANNOT change it.
