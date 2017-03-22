b05af520e10df90dc3cb269ac4dd19b9f661bace
Joseph Zoland <joeyzoland@gmail.com>
2017-03-21 19:23:50 -0700

    finished binary counter assignment

---
+++ IOS/Custom Cell/Custom Cell/CustomCell.swift
@@ -0,0 +1,17 @@
+//
+//  CustomCell.swift
+//  Custom Cell
+//
+//  Created by Joseph Zoland on 3/20/17.
+//  Copyright © 2017 Joseph Zoland. All rights reserved.
+//
+
+import UIKit
+
+class CustomCell: UITableViewCell {
+    
+    @IBOutlet weak var leftButton: UIButton!
+
+    @IBOutlet weak var rightLabel: UILabel!
+    
+}
