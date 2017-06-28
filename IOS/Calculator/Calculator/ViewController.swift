730c03b5d16953045d6e983e347ac953ae19fb52
Joseph Zoland <joeyzoland@gmail.com>
2017-06-27 18:58:54 -0700

    Updated calculator

--- IOS/Calculator/Calculator/ViewController.swift
+++ IOS/Calculator/Calculator/ViewController.swift
@@ -10,76 +10,87 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var operation = "false"
+    var operationDict = [1:"divide", 2:"multiply", 3:"subtract", 4:"add"]
+    var pastNumber = Double()
+    
     @IBOutlet weak var outputScreen: UILabel!
     
     @IBOutlet weak var CButton: UIButton!
     @IBOutlet weak var minusPlusButton: UIButton!
     @IBOutlet weak var percentageButton: UIButton!
     @IBOutlet weak var divideButton: UIButton!
-    @IBOutlet weak var multiplyButton: UIButton! 
+    @IBOutlet weak var multiplyButton: UIButton!
     @IBOutlet weak var subtractButton: UIButton!
     @IBOutlet weak var addButton: UIButton!
     @IBOutlet weak var equalsButton: UIButton!
     
-    @IBOutlet weak var sevenButton: UIButton!
-    @IBOutlet weak var eightButton: UIButton!
-    @IBOutlet weak var nineButton: UIButton!
-    @IBOutlet weak var fourButton: UIButton!
-    @IBOutlet weak var fiveButton: UIButton!
-    @IBOutlet weak var sixButton: UIButton!
-    @IBOutlet weak var oneButton: UIButton!
-    @IBOutlet weak var twoButton: UIButton!
-    @IBOutlet weak var threeButton: UIButton!
     @IBOutlet weak var zeroButton: UIButton!
     @IBOutlet weak var dotButton: UIButton!
     
     
+    @IBOutlet var allButtons: [UIButton]!
+    
+
+    @IBAction func oneToNineButtonsPressed(_ sender: UIButton) {
+        if (outputScreen.text == "0"){
+            outputScreen.text = String(sender.tag)
+        }
+        else {
+            outputScreen.text = outputScreen.text! + String(sender.tag)
+        }
+    }
+    
+    @IBAction func operationButtonPressed(_ sender: UIButton) {
+        operation = operationDict[sender.tag]!
+        pastNumber = Double(outputScreen.text!)!
+        outputScreen.text = "0"
+    }
+    
+    
     @IBAction func CButtonPressed(_ sender: UIButton) {
-        outputScreen.text = ""
+        operation = "false"
+        pastNumber = Double()
+        outputScreen.text = "0"
     }
     @IBAction func minusPlusButtonPressed(_ sender: UIButton) {
+        if outputScreen.text?.range(of: "-") == nil {
+            outputScreen.text = String("-") + outputScreen.text!
+        }
+        else {
+            let index = outputScreen.text?.index((outputScreen.text?.startIndex)!, offsetBy: 1)
+            outputScreen.text = outputScreen.text?.substring(from: index!)
+        }
     }
     @IBAction func percentageButtonPressed(_ sender: UIButton) {
+        let number = Double(outputScreen.text!)!/100
+        outputScreen.text = String(number)
     }
-    @IBAction func divideButtonPressed(_ sender: UIButton) {
-    }
-    @IBAction func multiplyButtonPressed(_ sender: UIButton) {
-    }
-    @IBAction func subtractButtonPressed(_ sender: UIButton) {
-    }
-    @IBAction func addButtonPressed(_ sender: UIButton) {
-    }
+
     @IBAction func equalsButtonPressed(_ sender: UIButton) {
+        if (operation != "false"){
+            if (operation == "divide") {
+                outputScreen.text = String(pastNumber / Double(outputScreen.text!)!)
+            }
+            else if (operation == "multiply") {
+                outputScreen.text = String(pastNumber * Double(outputScreen.text!)!)
+            }
+            else if (operation == "subtract") {
+                outputScreen.text = String(pastNumber - Double(outputScreen.text!)!)
+            }
+            else if (operation == "add") {
+                outputScreen.text = String(pastNumber + Double(outputScreen.text!)!)
+            }
+            if (Double(outputScreen.text!)!.truncatingRemainder(dividingBy: 1.0) == 0) {
+                let double = Double(outputScreen.text!)
+                let int = Int(double!)
+                outputScreen.text = String(int)
+            }
+        }
     }
     
     
-    @IBAction func sevenButtonPressed(_ sender: UIButton) {
-        outputScreen.text = outputScreen.text! + String(7)
-    }
-    @IBAction func eightButtonPressed(_ sender: UIButton) {
-        outputScreen.text = outputScreen.text! + String(8)
-    }
-    @IBAction func nineButtonPressed(_ sender: UIButton) {
-        outputScreen.text = outputScreen.text! + String(9)
-    }
-    @IBAction func fourButtonPressed(_ sender: UIButton) {
-        outputScreen.text = outputScreen.text! + String(4)
-    }
-    @IBAction func fiveButtonPressed(_ sender: UIButton) {
-        outputScreen.text = outputScreen.text! + String(5)
-    }
-    @IBAction func sixButtonPressed(_ sender: UIButton) {
-        outputScreen.text = outputScreen.text! + String(6)
-    }
-    @IBAction func oneButtonPressed(_ sender: UIButton) {
-        outputScreen.text = outputScreen.text! + String(1)
-    }
-    @IBAction func twoButtonPressed(_ sender: UIButton) {
-        outputScreen.text = outputScreen.text! + String(2)
-    }
-    @IBAction func threeButtonPressed(_ sender: UIButton) {
-        outputScreen.text = outputScreen.text! + String(3)
-    }
+
     @IBAction func zeroButtonPressed(_ sender: UIButton) {
         if (outputScreen.text != "0"){
             outputScreen.text = outputScreen.text! + String(0)
@@ -98,7 +109,12 @@ class ViewController: UIViewController {
     
     
     override func viewDidLoad() {
-        outputScreen.text = ""
+        for i in allButtons {
+//            i.layer.cornerRadius = 5
+            i.layer.borderWidth = 1
+            i.layer.borderColor = UIColor.black.cgColor
+        }
+        outputScreen.text = "0"
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
